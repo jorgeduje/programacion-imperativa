e9de0919d0c9b97318f27f0bba0e363d60874da9
jorgeduje <jorgeduje4@gmail.com>
2022-03-08 21:29:59 -0300

    variables

---
+++ variables.js
@@ -0,0 +1,54 @@
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+const apellido = "Duje" // string o cadena de texto
+
+let edad = 29 // number
+
+let estaVivo = true // booleano
+
+
+
+console.log( edad, apellido, estaVivo )
+
+let nombre = prompt("inserte su nombre")
+
+console.log('el nombre es: ' + nombre + " y la edad es de : " + edad + " años")
+ 
+// TEMPLATE LITTERALS O BACKSTICK
+
+console.log(`el nombre es ${nombre} y la edad es de ${edad} años`)
+
+// operador de asignacion ( = )
+
+// operadores aritmeticos ( +, -, *, /)
+ // ejemplo
+ // let multiplicacion = 2 * 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
